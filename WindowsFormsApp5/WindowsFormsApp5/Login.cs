172b137360da4a8774cfd323585c59cbecd26e32
aliyaabdulwaheed <aliyaabdulwaheed7@gmail.com>
2026-02-20 17:17:31 +0500

    Add files via upload

---
+++ WindowsFormsApp5/WindowsFormsApp5/Login.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (usertxt.Text == "admin")
+            {
+                passtxt.Enabled = true;
+            }
+            else { 
+
+                passtxt.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (usertxt.Text == "admin" && passtxt.Text == "admin123")
+            {
+                Dashboard dashboard = new Dashboard();
+                dashboard.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("invalid password");
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
